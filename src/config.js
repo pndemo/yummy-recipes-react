cfa963108c4d37f3030971269e4db8faf210abbc
pndemo <ndemopaul1@gmail.com>
2018-03-12 15:55:31 +0300

    [Chore #155896966] Setup React App

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// API and App URLs
+export const baseAPIURL = 'http://127.0.0.1:5000';
+export const authAPIURL = baseAPIURL.concat( '/api/v1/auth/' );
+export const categoryAPIURL = baseAPIURL.concat( '/api/v1/category/' );
+export const recipeAPIURL = baseAPIURL.concat( '/api/v1/recipe/' );
